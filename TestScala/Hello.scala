a03daf0a25b5bacf510699a15f987c20a89d13af
Ankur <ultimate.dragforce@gmail.com>
2015-09-06 14:55:30 -0400

    Made it Loop the Loop

--- TestScala/Hello.scala
+++ TestScala/Hello.scala
@@ -3,6 +3,7 @@ import java.security.MessageDigest
 
 import akka.actor._
 import akka.actor.ActorSystem
+import scala.util
 
 
 class Master extends Actor {
@@ -22,14 +23,23 @@ class Master extends Actor {
 
  object Main extends App {
   
-	println ("Hi")
+	println ("Running: ")
 	
 	sha256("ankurbagchi")
 	
+	
+	while (true) {
+    val r = new scala.util.Random
+    val sb = new StringBuilder
+    for (i <- 1 to 10) {
+      sb.append(r.nextPrintableChar)
+    }
+    sha256 ("ankurbagchi"+sb.toString)
+  }
 
 def sha256(base:String) : String = {
     
-		val system = ActorSystem("SenderSystem")
+		//val system = ActorSystem("SenderSystem")
 		
 		//val senderActor = system.actorOf(Props[Master], name = "senderActor")
 		
@@ -45,6 +55,22 @@ def sha256(base:String) : String = {
             hexString.append(hex)
         }
 		
+		var s = hexString.toString();
+		
+		var difficulty = 6;
+	
+		var isDif = 1;
+		
+		for ( i <- 0 to (difficulty-1))
+		{
+		
+		if (s.charAt(i) != '0')
+			isDif = 0;
+		
+		}
+		
+		if (isDif == 1)
+		println (base + " " + s)
 		
 		//senderActor ! hexString.toString()
 		
