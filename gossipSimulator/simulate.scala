9ba68d0b78a0d30f40beedfe2694a31d1994bb9a
Saravanan SJ <sjs7007@gmail.com>
2015-10-01 16:25:16 -0400

    4

--- gossipSimulator/simulate.scala
+++ gossipSimulator/simulate.scala
@@ -1,19 +1,33 @@
 import akka.actor._
 import scala.math._ //for absolute value
+import scala.collection.mutable.ListBuffer //for storing neighbor list : https://www.cs.helsinki.fi/u/wikla/OTS/Sisalto/examples/html/ch17.html
+import scala.util._ //for random number
 
 object Simulate extends App {
   
-  case class gossipMsg(Double s,Double w)
+  val system = ActorSystem("Gossip")
+  val node1 = system.actorOf(Props[Node],name="node")
+  val node2 = system.actorOf(Props[Node],name="node2")
+  //node1.neighborList += node2
+  //node2.neighborList += node1
+  node1 ! addNeighbor(node2)
+  node2 ! addNeighbor(node1)
+  node1 ! start 
+  
+  case class gossipMsg(s: Double,w: Double)
+  case class start
+  case class addNeighbor(x: ActorRef)
 
-  class Node(id: Int) extends Actor {
+  class Node(id: Double) extends Actor {
     var nodeId = id //actor number
-    var s = nodeId 
-    var w = 1
-    var ratio = s/w
+    var s : Double= nodeId 
+    var w : Double= 1
+    var ratio : Double= s/w
     var gossipRecCount=0 // used for termination condition
     var ratioChange=0 //used for termination condition
     var rumorCount=0
     var change = new Array[Double](3)
+    var neighborList = new ListBuffer[ActorRef]
 
     def shouldITerminate() = {
       var newRatio = s/w
@@ -23,7 +37,7 @@ object Simulate extends App {
 
       if(gossipRecCount>=3) { //if difference is 
         var sumChange = change(0)+change(1)+change(2)
-        if(sumChange<0.0001) {
+        if(sumChange<Math.pow(10,-10)) {
           // terminate the actor
           context.stop(self)
         }
@@ -32,13 +46,29 @@ object Simulate extends App {
 
     def receive = {
       case gossipMsg(s_r,w_r) =>
+        println("Received gossip message. ratio = "+ratio)
         s = s + s_r
         w = w + w_r
         rumorCount = rumorCount+1
         shouldITerminate()
 
         //select neighbor and send gossip if rumorCount <= 10
-        
-    }
+        if(rumorCount<10) {
+          s=s/2
+          w=w/2
+          ratio=s/w
+          println("Forwarding gossip message. ratio = "+ratio)
+          neighborList(Random.nextInt(neighborList.length)) ! gossipMsg(s,w)
+        }
+
+      case start =>
+        s=s/2
+        w=w/2
+        rumorCount=rumorCount+1
+        neighborList(Random.nextInt(neighborList.length)) ! gossipMsg(s,w)
+
+      case addNeighbor(x) =>
+        neighborList+= x
+    }  
   }
 }
