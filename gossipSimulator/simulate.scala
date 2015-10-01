bd92bca565ca387902bd4acaa069e27179b0c914
Saravanan SJ <sjs7007@gmail.com>
2015-10-01 10:51:48 -0400

    2

--- gossipSimulator/simulate.scala
+++ gossipSimulator/simulate.scala
@@ -1,43 +1,45 @@
 import akka.actor._
+import scala.math._ //for absolute value
 
-case class gossipMsg(Double s,Double w,String msg)
+object Simulate extends App {
+  
+  case class gossipMsg(Double s,Double w)
 
-class Node(id: Int) extends Actor {
-  var nodeId = id //actor number
-  var s = nodeId 
-  var w = 1
-  var ratio = s/w
-  var gossipRecCount=0 // used for termination condition
-  var ratioChange=0 //used for termination condition
-  var rumorCount=0
+  class Node(id: Int) extends Actor {
+    var nodeId = id //actor number
+    var s = nodeId 
+    var w = 1
+    var ratio = s/w
+    var gossipRecCount=0 // used for termination condition
+    var ratioChange=0 //used for termination condition
+    var rumorCount=0
 
-  def shouldITerminate() = {
-    gossipRecCount = gossipRecCount + 1
-    var newRatio = s/w
-    ratioChange = ratioChange + (newRatio - ratio)
-    ratio = newRatio
+    def shouldITerminate() = {
+      gossipRecCount = gossipRecCount + 1
+      var newRatio = s/w
+      ratioChange = ratioChange + math.abs(newRatio - ratio)
+      ratio = newRatio
 
-    if(gossipRecCount%3==0) {
-      if(ratioChange<0.0001) {
-        // terminate the actor
-        context.stop(self)
-      }
-      else {
-        ratioChange=0
+      if(gossipRecCount%3==0) { //if difference is 
+        if(ratioChange<0.0001) {
+          // terminate the actor
+          context.stop(self)
+        }
+        else {
+          ratioChange=0
+        }
       }
     }
+
+    def receive = {
+      case gossipMsg(s_r,w_r) =>
+        s = s + s_r
+        w = w + w_r
+        rumorCount = rumorCount+1
+        shouldITerminate()
+
+        //select neighbor and send gossip if rumorCount <= 10
+        
+    }
   }
-
-  def receive = {
-    case gossipMsg(s_r,w_r) =>
-      s = s + s_r
-      w = w + w_r
-      rumorCount = rumorCount+1
-      shouldITerminate()
-
-      //select neighbor and send gossip if rumorCount <= 10
-      
-  }
-
-
 }
