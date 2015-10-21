3971c6684a56ed78fcf6d43a00db52a12c39edd4
Saravanan SJ <sjs7007@gmail.com>
2015-10-21 16:35:46 -0400

    merged

--- chordProtocol/Chord.scala
+++ chordProtocol/Chord.scala
@@ -6,24 +6,41 @@ import scala.util._ //for random number
 object Chord extends App {
   val system = ActorSystem("Chord")
   //var x : Node = new Node(2)
-  val tmp = system.actorOf(Props(new Node(2)))  
-  //println(x.nodeId) 
-  tmp ! "hello"
+  val tmp = system.actorOf(Props(new Node(1,null))) 
+
+  val tmp2 = system.actorOf(Props(new Node(2,tmp))) 
+
+  tmp ! getPredecessor(2)
+  
+ // val tmp3 = system.actorOf(Props(new Node(3,1))) 
+  
 }
 
-class fingerData {
-  var nodeId = -1
-  var actorRefence : ActorRef = null
-  var nodeReference : Node = null
+class fingerData(id: Int, x: ActorRef, y: Node) {
+  var nodeId : Int = id
+  var actorReference : ActorRef = x
+  var nodeReference : Node = y
 }
 
-class Node(id: Int) extends Actor {
+case class getPredecessor(id: Int)
+
+class Node(id: Int , source: ActorRef) extends Actor {
   var m = 8
   var nodeId = id
   var successor : Node = null
   var predecessor : Node = null
   var fingerTable : Array[fingerData] = new Array[fingerData](m)
   var actorReference : ActorRef = null
+  if(source==null) {
+    /*successor = this
+    predecessor = this
+    for(i<- 0 until m) {
+      fingerTable(i) = new fingerData(this.nodeId,this.actorReference,this)
+    }*/
+    initFingerTable(source)
+
+  }
+
 
   //ask node n to find id's successor
   def findSuccessor(id: Int) : Node = {
@@ -34,14 +51,20 @@ class Node(id: Int) extends Actor {
 
   //ask node n to find id's predecessor
   def findPredecessor(id: Int) : Node = {
+    println("here")
     var nDash : Node = this 
-    while(!In(id,nDash.nodeId,nDash.successor.nodeId, false, true)) { //id not in (nDash,nDash.succ] 
+    println("here2")
+    println(nDash.nodeId)
+    println(nDash.successor.nodeId)
+    if(!In(id,nDash.nodeId,nDash.successor.nodeId, false, true)) { //id not in (nDash,nDash.succ] 
+      println("here3")
       nDash = nDash.closestPrecedingFinger(id) 
     }
+    println("predecessor is "+nDash.nodeId)
     return nDash
   }
 
-  //In
+    //In
   def In(x:Int, lower:Int,higher:Int, includeLower: Boolean, includeUpper: Boolean) : Boolean = {
   
   var a = 0
@@ -66,28 +89,29 @@ class Node(id: Int) extends Actor {
     }
     else for(i <- 0 to (m-1)) { // n is the only node in the network
       fingerTable(i).nodeId = id
-      fingerTable(i).actorRefence = this
+      fingerTable(i).actorReference = this.actorReference
       fingerTable(i).nodeReference = this
     }
   }
   
   // Init Finger Table
   def initFingerTable (nDash: Node){
-    fingerTable(0) = nDash.successor
+    fingerTable(0).nodeReference = nDash.successor
+    fingerTable(0).actorReference = nDash.successor.actorReference
     predecessor = nDash.successor.predecessor
     successor.predecessor = this
   
     for (i <- 0 to (m-2)) {
-      if (In(id + scala.math.pow(2,i+1), id, fingerTable(i).nodeId, true, false)) {
+      if (In(id + scala.math.pow(2,i+1).toInt, id, fingerTable(i).nodeId, true, false)) {
         fingerTable(i+1).nodeId = fingerTable(i).nodeId
-        fingerTable(i+1).actorRefence = fingerTable(i).actorRefence
+        fingerTable(i+1).actorReference = fingerTable(i).actorReference
         fingerTable(i+1).nodeReference =  fingerTable(i).nodeReference
         }
       else {
-        var newNode = nDash.findSuccessor(fingerTable(i+1).nodeReference)
+        var newNode = nDash.findSuccessor(fingerTable(i+1).nodeId)
         fingerTable(i+1).nodeId = newNode.nodeId
-        fingerTable(i+1).actorRefence = newNode.actorRefence
-        fingerTable(i+1).nodeReference =  newNode.nodeReference
+        fingerTable(i+1).actorReference = newNode.actorReference
+        fingerTable(i+1).nodeReference = newNode
       }
     }
   }
@@ -97,6 +121,7 @@ class Node(id: Int) extends Actor {
   
   }
 
+ 
   //return closest finger preceding id
   def closestPrecedingFinger(id: Int) : Node = {
     var i : Int =0
@@ -105,6 +130,7 @@ class Node(id: Int) extends Actor {
         return fingerTable(i).nodeReference
       }
     }
+    println("here4")
     return this
   }
   
@@ -115,8 +141,8 @@ class Node(id: Int) extends Actor {
       println("ds")
       closestPrecedingFinger(2)
 
-    case 
-  }
-  
-  
+    case getPredecessor(id: Int) =>
+      findPredecessor(id)
+
+  }  
 }
