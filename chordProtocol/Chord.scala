64a40a9ff4f73cce6a2e2041836d59938c95be2e
Ankur <ultimate.dragforce@gmail.com>
2015-10-21 00:28:56 -0400

    Added functions

--- chordProtocol/Chord.scala
+++ chordProtocol/Chord.scala
@@ -35,18 +35,66 @@ class Node(id: Int) extends Actor {
   //ask node n to find id's predecessor
   def findPredecessor(id: Int) : Node = {
     var nDash : Node = this 
-    while(notIn(id,nDash.nodeId,nDash.successor.nodeId)) { //id not in (nDash,nDash.succ] 
+    while(!In(id,nDash.nodeId,nDash.successor.nodeId, false, true)) { //id not in (nDash,nDash.succ] 
       nDash = nDash.closestPrecedingFinger(id) 
     }
     return nDash
   }
 
-  //notIn
-  def notIn(x:Int, lower:Int,higher:Int) : Boolean = {
-    if(x>lower && x <=higher) {
-      return false
+  //In
+  def In(x:Int, lower:Int,higher:Int, includeLower: Boolean, includeUpper: Boolean) : Boolean = {
+  
+  var a = 0
+  var b = 0
+  
+  if (includeLower)
+    a = 1
+    
+   if (includeUpper)
+    b = 1
+  
+    if(lower < (x+a) && (x-b) < higher)
+      return true
+    return false
+  }
+  
+  // Join
+  def join (nDash: Node) {
+    if (nDash != null) {
+      initFingerTable(nDash)
+      updateOthers(nDash)
     }
-    return true
+    else for(i <- 0 to (m-1)) { // n is the only node in the network
+      fingerTable(i).nodeId = id
+      fingerTable(i).actorRefence = this
+      fingerTable(i).nodeReference = this
+    }
+  }
+  
+  // Init Finger Table
+  def initFingerTable (nDash: Node){
+    fingerTable(0) = nDash.successor
+    predecessor = nDash.successor.predecessor
+    successor.predecessor = this
+  
+    for (i <- 0 to (m-2)) {
+      if (In(id + scala.math.pow(2,i+1), id, fingerTable(i).nodeId, true, false)) {
+        fingerTable(i+1).nodeId = fingerTable(i).nodeId
+        fingerTable(i+1).actorRefence = fingerTable(i).actorRefence
+        fingerTable(i+1).nodeReference =  fingerTable(i).nodeReference
+        }
+      else {
+        var newNode = nDash.findSuccessor(fingerTable(i+1).nodeReference)
+        fingerTable(i+1).nodeId = newNode.nodeId
+        fingerTable(i+1).actorRefence = newNode.actorRefence
+        fingerTable(i+1).nodeReference =  newNode.nodeReference
+      }
+    }
+  }
+  
+  // Update Others
+  def updateOthers (nDash: Node) {
+  
   }
 
   //return closest finger preceding id
@@ -59,10 +107,14 @@ class Node(id: Int) extends Actor {
     }
     return this
   }
+  
+  
 
   def receive = {
     case "hello" => 
       println("ds")
       closestPrecedingFinger(2)
   }
+  
+  
 }
