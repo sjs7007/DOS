356c3b22ec6126079c0ac6028e89c1a9baf6dd51
Saravanan SJ <sjs7007@gmail.com>
2015-12-14 02:44:34 -0500

    AES with iv and padding

--- fbProject/crypto/java/AESTest.java
+++ fbProject/crypto/java/AESTest.java
@@ -1,7 +1,44 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.SecureRandom;
 import javax.crypto.*;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 
+
+
+//AES with padding and iv
+public class AESTest {
+  public static void main(String[] args) throws Exception {
+    KeyGenerator KeyGen = KeyGenerator.getInstance("AES");
+    KeyGen.init(128);
+
+    SecretKey SecKey = KeyGen.generateKey();
+
+    Cipher AesCipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+
+
+    SecureRandom randomNumberGenerator  = new SecureRandom();
+    byte bytes[] = new byte[16];
+    randomNumberGenerator.nextBytes(bytes);
+
+    byte[] byteText = "Your Plain Text Here".getBytes();
+
+    IvParameterSpec initializationVector = new IvParameterSpec(bytes);
+
+
+    AesCipher.init(Cipher.ENCRYPT_MODE, SecKey,initializationVector);
+    byte[] encryptedData = AesCipher.doFinal(byteText);
+
+
+    AesCipher.init(Cipher.DECRYPT_MODE, SecKey,initializationVector);
+    byte[] decryptedData = AesCipher.doFinal(encryptedData);
+    String temp = new String(decryptedData,"UTF-8");
+    System.out.println("Decrytped Data : "+temp);
+  }
+}
+
+/*AES without IV
 public class AESTest {
   public static void main(String[] args) throws Exception {
     //String FileName = "encryptedtext.txt";
@@ -31,3 +68,4 @@ public class AESTest {
     System.out.println("Decrytped Data : "+temp);
   }
 }
+*/
